89e00cd22a0b620d3097ea2f15ac8e50fd8ae9df
Shaohua Yuan <shyuan@tamu.edu>
2019-12-10 13:57:52 -0800

    Done with https://tamuber-vehicles-19fall.herokuapp.com/

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,3 +31,8 @@ Driver.create([
 	{ name: "Scottie Payton" },
 	{ name: "Jerry  Griffin" },
 ])
+
+
+connection = ActiveRecord::Base.connection();
+# Execute a sql statement
+connection.execute("INSERT INTO users (email, encrypted_password, reset_password_token, reset_password_sent_at, remember_created_at, created_at, updated_at, name, admin, license, tel, address, confirmation_token, confirmed_at, confirmation_sent_at, unconfirmed_email) VALUES ('admin@tamu.edu', '$2a$11$mP6M8u.Q/d8JXJ21Drq3UevuW26/1Mutz.7jU3qvWx1sWkj2mK6oW', null, null, null, '2019-12-10 21:49:40.395501', '2019-12-10 21:50:01.816273', 'admin', 0, null, null, null, 'xSVwSWPpGFREbAHN9hp3', '2019-12-10 21:50:01.815552', '2019-12-10 21:49:40.395610', null);")
